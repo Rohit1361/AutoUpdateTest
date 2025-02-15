bb261a0ea5dfcb6de62178a4afcb71824e9bcd45
Rohit Swami <rohitswami1361@gmail.com>
2025-02-15 14:52:33 +0530

    update2

--- TESTSETUP/TESTSETUP/Form1.Designer.cs
+++ TESTSETUP/TESTSETUP/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 33);
             this.label1.TabIndex = 0;
-            this.label1.Text = "2";
+            this.label1.Text = "3";
             // 
             // Form1
             // 
